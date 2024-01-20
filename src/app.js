812403231f415bfd90be4f111281b0be541658d7
juanquintero <juanestquintero@gmail.com>
2024-01-19 23:07:20 -0500

    fix(contracts): get by id and profile id

--- src/app.js
+++ src/app.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model')
+const { Op } = require('sequelize')
+const { getProfile } = require('./middleware/getProfile')
+
 const app = express();
+
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
+
+
+app.use(getProfile);
 
 /**
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
+app.get('/contracts/:id', async (req, res) => {
+    const { Contract } = req.app.get('models')
+    const { id: contractId, } = req.params
+    const profileId = req.profile.id;
+
+    const contract = await Contract.findOne({
+        where: {
+            id: parseInt(contractId),
+            [Op.or]: [{ ClientId: profileId }, { ContractorId: profileId }]
+        }
+    });
+
+    if (!contract) return res.status(404).end()
     res.json(contract)
 })
+
 module.exports = app;

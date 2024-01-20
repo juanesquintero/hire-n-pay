d1da58158b632be6d3f7e7f6ad7b683116c4d0b1
juanquintero <juanestquintero@gmail.com>
2024-01-20 00:17:21 -0500

    feat(jobs): post pay job of active contract

--- src/routers/jobs.js
+++ src/routers/jobs.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const { Op } = require('sequelize')
 
 /**
  * GET /unpaid
@@ -29,5 +28,56 @@ router.get('/unpaid', async (req, res) => {
     if (!jobs) return res.status(404).end()
     res.json(jobs)
 })
+
+router.post('/:job_id/pay', async (req, res, next) => {
+    const { Job, Contract, Profile } = req.app.get('models')
+    const sequelize = req.app.get('sequelize')
+    const jobId = req.params.job_id;
+    const client = req.profile;
+
+    if (client?.type !== 'client') {
+        return res.status(403).send('Forbidden');
+    }
+
+    const job = await Job.findByPk(jobId, { include: [Contract] });
+    if (!job || job?.Contract?.status !== 'in_progress') {
+        return res.status(404).send('Job not found or not active');
+    }
+    if (job?.paid) {
+        return res.status(400).send('Job already paid');
+    }
+
+    const contractor = await Profile.findByPk(job?.Contract?.ContractorId);
+    if (!contractor) {
+        return res.status(404).send('Contrator not found');
+    }
+
+    if (client.balance < job.price) {
+        return res.status(400).send('Insufficient balance');
+    }
+
+    // Start a transaction
+    const transaction = await sequelize.transaction();
+
+    try {
+        client.balance -= job.price;
+        contractor.balance += job.price;
+        job.paid = true;
+        job.paymentDate = new Date();
+
+        await client.save({ transaction });
+        await contractor.save({ transaction });
+        await job.save({ transaction });
+
+        // Commit the transaction
+        await transaction.commit();
+        res.send('Job has been paid successfully!');
+    } catch (error) {
+        // rollback the transaction if an error occurred
+        await transaction.rollback();
+        throw error;
+    }
+
+});
 
 module.exports = router
